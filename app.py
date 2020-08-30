d7a5124c3ff5aa3a138d5e8c4c028884d9464a31
Oscar Neira <oneirag@yahoo.es>
2020-08-30 05:00:47 +0200

    Port is configurable in command line. Added a blueprint to change all routes of the server if needed

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+import argparse
 
-app = Flask(__name__)
+bp = Blueprint("euro_game", __name__,
+                        template_folder='templates')
 
 
-@app.route('/')
+@bp.route('/')
 def coin_game():
     return render_template("euro_coin_game.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", default=5555, help="flask port")
+    parser.add_argument("--url_prefix", default="/", help="Url prefix for all routes")
+    args, _ = parser.parse_known_args()
+    app = Flask(__name__)
+    app.register_blueprint(bp, url_prefix=args.url_prefix)
+    app.run(port=args.port)
