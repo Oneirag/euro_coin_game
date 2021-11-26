b08759b6a3d00d57fad5e5f1aecba03b90cf62dc
Oscar Neira <oneirag@yahoo.es>
2021-11-26 05:59:09 +0100

    Removed flask dependency, now pure js+html Migrated from bootstrap v4 to v5 Added README.md

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # euro_coin_game
-Simple flask html+js page to teach kids to deal with euro coins and notes. Click in the coins/notes to match the expected cash amount.
+Simple Vanilla JS + Bootstrap5 html page to teach kids to deal with euro coins and notes. 
+Click in the coins/notes to match the expected cash amount.
 Watch it live in [my page](https://www.neirapinuela.es/euro_game/)
+
+To use it just open euro_coin_game.html. It can be integrated in a web server, just removing this README.md
--- static/coin_game.js
+++ static/coin_game.js
@@ -1,11 +1,29 @@
 let coins= [];
 
+function create_coin_notes() {
+    // Fills #coins and #notes divs with the corresponding images and onclick functions
+    let coins_html = "";
+    let notes_html = "";
+
+    for (const coin of ["0001", "0002", "0005", "0010", "0020", "0050", "0100", "0200"]) {
+        coins_html += '<img src="static/' + coin + '.jpg" onclick="javascript:add_coin(\'' +
+            coin + '\')" width="80px" height="80px">';
+
+        document.getElementById("coins").innerHTML = coins_html;
+    }
+    for (const note of ["0500", "1000", "2000", "5000", "10000", "20000", "50000"]) {
+        notes_html += '<img src="static/' + note + '.jpg" onclick="javascript:add_coin(\'' +
+            note + '\')" width="160px" height="' + Math.round(160*(62/120)) + 'px">';
+        document.getElementById("notes").innerHTML = notes_html;
+    }
+}
+
 function refresh_plot(){
     let coins_html = "";
     coins.forEach(function (coin) {
         coins_html += "<img src='static/" + coin + ".jpg' width='100'>";
     });
-    $("#coin_canvas").html(coins_html);
+    document.getElementById("coin_canvas").innerHTML = coins_html;
 }
 
 function add_coin(coin){
@@ -22,14 +40,14 @@ function pop_coins(){
 function reset_coins(reset_target=true){
     coins = [];
     if (reset_target) {
-        coin_range = $("#select_coin_range").val().split(";");
+        coin_range = document.getElementById("select_coin_range").value.split(";");
         let min = coin_range[0];
         let max = coin_range[1];    // In euros
         console.log("Val es " + String(coin_range));
         console.log("Min es " + String(min));
         console.log("Max es " + String(max));
         random_number = Math.max(Math.random() * (max - min) + min, min);
-        $("#target_coins").val(Number(random_number).toFixed(2) + "€");
+        document.getElementById("target_coins").value = Number(random_number).toFixed(2) + "€";
     }
     refresh_plot();
 }
@@ -39,18 +57,19 @@ function check_coins(){
     coins.forEach(function (coin) {
         sum += parseInt(coin) / 100;
     });
-    target_value = $("#target_coins").val();
+    target_value = document.getElementById("target_coins").value;
     console.log("La suma es " + String(sum) + "€ y el objetivo era: " + String(target_value));
     if (target_value == Number(sum).toFixed(2) + "€") {
-        $("#modal_ok").modal(true);
+        var myModal = new bootstrap.Modal(document.getElementById('modal_ok'))
+        myModal.show();
         reset_coins();
     }else{
-        $("#modal_no_ok").modal(true);
+        var myModal = new bootstrap.Modal(document.getElementById('modal_no_ok'))
+        myModal.show();
     }
 }
 
-
-$( document ).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
     reset_coins();
 });
 
